7f9c3132f18237d2ed9b9db8ad737418bf6c1d45
mmuffins <mail@tiv73.com>
2024-05-30 11:01:19 +0200

    Fix exception if file as not sort_artist

--- artist_resolver/TrackManager.py
+++ artist_resolver/TrackManager.py
@@ -5,3 +5,4 @@
             if cv_pattern.match(reordered_data[i]["joinphrase"]) and reordered_data[
                 i + 1
             ]["joinphrase"].startswith(")"):
+        self.custom_name = sort_name.replace(",", "") if sort_name else sort_name
