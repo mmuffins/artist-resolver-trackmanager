36cf91d8d0b2dfd77f5c8779c9c1b684f7584229
mmuffins <mail@tiv73.com>
2024-05-30 11:28:23 +0200

    test

--- artist_resolver/TrackManager.py
+++
@@ -1,8 +0,0 @@
-                "joinphrase" not in reordered_data[i].keys()
-                or "joinphrase" not in reordered_data[i + 1].keys()
-                continue
-
-            if cv_pattern.match(reordered_data[i]["joinphrase"]) and reordered_data[
-                i + 1
-            ]["joinphrase"].startswith(")"):
-        self.custom_name = sort_name.replace(",", "") if sort_name else sort_name
