a139a58313c94082392883bf334e875e642b713e
Michael Kellner <mail@tiv73.com>
2024-09-01 12:05:32 +0200

    Auto-increment version (#20)
    * Use latest python version in actions
    
    * Automatically bump versions when creating a PR

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements/common.txt") as f:
 
 setup(
     name="artist-resolver-trackmanager",
-    version="2.0.4",
+    version="2.0.101",
     author="mmuffins",
     description="Python library for the artist resolver api",
     url="https://github.com/mmuffins/artist-resolver-trackmanager",
